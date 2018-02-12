aeb1cbbd95cbf5469e1cee3acde1cbc5049f6120
sxuboss <sxuboss@163.com>
2018-02-12 10:05:20 +0800

    add jms

--- EurekaCilent2/src/main/java/com/feign/demo/service/impl/Client2ServiceImpl.java
+++ EurekaCilent2/src/main/java/com/feign/demo/service/impl/Client2ServiceImpl.java
@@ -2,6 +2,8 @@ package com.feign.demo.service.impl;
 
 import com.feign.demo.service.Client2Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,11 +22,19 @@ public class Client2ServiceImpl implements Client2Service {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("server:.port")
+    private String prot;
+
 
     @Override
     public void sayHello(String name ) {
                String forObject = restTemplate.getForObject("http://SERVICEHI/hi?name="+name, String.class);
         System.out.println("forObject"+forObject);
 
+    }
+
+    @JmsListener(destination = "FeignQueue")
+    public void reviceMsg(String text){
+        System.out.println("I am ffom  "+ prot+ " : 接受到消息为： "+text);
     }
 }
